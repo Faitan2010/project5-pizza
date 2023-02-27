6ccc5d2d672ffa4f1d79f5b98f46e2fd9c7de39d
Faitan2010 <jenia1800@gmail.com>
2023-02-27 23:52:11 +0200

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ import { Anchors } from "./anchor.js";
 import { Menu } from "./menu.js";
 import { Switcher } from "./switcher.js";
 
+import * as flsFunctions from "./modules//functions.js"
+
+flsFunctions.isWebp();
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     new Switcher();
     new Anchors();
